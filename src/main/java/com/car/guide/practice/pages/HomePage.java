4145faa4a398e4850f2efa87ab01f9fd57a7283a
Mathematics13 <haripriyakrishnan4@gmail.com>
2023-12-20 22:33:40 +0000

    Wednesday HW Week15

---
+++ src/main/java/com/car/guide/practice/pages/HomePage.java
@@ -0,0 +1,44 @@
+package com.car.guide.practice.pages;
+
+import com.car.guide.practice.utility.Utility;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends Utility
+{
+    private static final Logger log = LogManager.getLogger(NewAndUsedCarSearchPage.class.getName());
+    @CacheLookup
+    @FindBy(linkText = "buy + sell")
+    WebElement buyAndSellLink;
+
+    @CacheLookup
+    @FindBy(xpath = "//a[text()='Search Cars']")
+    WebElement carSearchLink;
+
+    @CacheLookup
+    @FindBy(xpath = "//a[text()='Used']")
+    WebElement usedCarLink;
+
+    @CacheLookup
+    @FindBy(xpath = "//a[contains(text(),'Find a Dealer')]")
+    WebElement findDealerLink;
+
+    public void mouseHoverOnBuyAndSellLink(){
+        mouseHoverToElement(buyAndSellLink);
+    }
+    public void clickOnCarSearchLink(){
+        clickOnElement(carSearchLink);
+    }
+    public void clickOnUsedCarLink(){
+        clickOnElement(usedCarLink);
+    }
+
+    public void clickOnFindDealerLink()
+    {
+        clickOnElement(findDealerLink);
+    }
+}
+
---
+++ src/test/java/com/car/guide/practice/steps/FindDealersSteps.java
@@ -0,0 +1,33 @@
+package com.car.guide.practice.steps;
+
+import com.car.guide.practice.pages.HomePage;
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import java.util.List;
+
+public class FindDealersSteps {
+    @And("I click ‘Find a Dealer’")
+    public void iClickFindADealer()
+    {
+          new HomePage().clickOnFindDealerLink();
+    }
+
+    @Then("I navigate to ‘car-dealers’ page")
+    public void iNavigateToCarDealersPage()
+    {
+
+    }
+
+    @And("I should see the dealer names <dealersName> are display on page")
+    public void iShouldSeeTheDealerNamesDealersNameAreDisplayOnPage(DataTable dataTable)
+    {
+        List<List<String>> dealersName  = dataTable.asLists(String.class);
+        for ( List<String>  name  :  dealersName)
+        {
+            System.out.println(name);
+            //System.out.println(list.get(0) );
+
+        }
+    }
+}
